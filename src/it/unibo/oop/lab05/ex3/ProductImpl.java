e651d09ba20bb3e713e7e56a51a9796763d01da1
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2020-11-11 19:14:04 +0100

    Fix ProductImpl

--- src/it/unibo/oop/lab05/ex3/ProductImpl.java
+++ src/it/unibo/oop/lab05/ex3/ProductImpl.java
@@ -22,10 +22,51 @@ public class ProductImpl implements Product {
 	}
 
 
-	@Override
+
 	public String toString() {
 		return "Product [name=" + name + ", quantity=" + quantity + "]";
 	}
+
+
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ProductImpl other = (ProductImpl) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+
+//    public int hashCode() {
+//        return name.hashCode();
+//    }
+//
+//    
+//    public boolean equals(final Object obj) {
+//        if (obj instanceof Product) {
+//            final var pr = (Product) obj;
+//            return name.equals(pr.getName());
+//        }
+//        return false;
+//    }
+    
+    
 	
 	
 
